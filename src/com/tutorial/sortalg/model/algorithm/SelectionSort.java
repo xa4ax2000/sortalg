a614cab6f5504c8be6f89e6d357c426b958a0c5b
Andrew Hyun <andrew.hyun@alumni.stonybrook.edu>
2019-11-21 15:57:28 -0500

    SelectionSort Implementation

--- src/com/tutorial/sortalg/model/algorithm/SelectionSort.java
+++ src/com/tutorial/sortalg/model/algorithm/SelectionSort.java
@@ -1,4 +1,31 @@
 package com.tutorial.sortalg.model.algorithm;
 
+import com.tutorial.sortalg.model.SortingNumber;
+
+import java.util.List;
+
 public class SelectionSort implements SortingAlgorithm {
+    @Override
+    public String getAlgorithm() {
+        return "SELECTION SORT";
+    }
+
+    @Override
+    public SortingNumber[] sort(SortingNumber[] data) {
+        for(int i = 0; i < data.length; i++){
+            int minimumIndex = i;
+
+            for(int j = i+1; j < data.length; j++){
+                if(data[minimumIndex].getSortingValue() > data[j].getSortingValue()){
+                    minimumIndex = j;
+                }
+            }
+
+            SortingNumber temp = data[i];
+            data[i] = data[minimumIndex];
+            data[minimumIndex] = temp;
+        }
+
+        return data;
+    }
 }

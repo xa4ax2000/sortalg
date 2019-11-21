be4628172e5579a65e6ceb8614b72ccb1aa48fd5
Andrew Hyun <andrew.hyun@alumni.stonybrook.edu>
2019-11-21 16:13:00 -0500

    Improved selection sort computation by handling edge case of size 1

--- src/com/tutorial/sortalg/model/algorithm/SelectionSort.java
+++ src/com/tutorial/sortalg/model/algorithm/SelectionSort.java
@@ -12,7 +12,7 @@ public class SelectionSort implements SortingAlgorithm {
 
     @Override
     public SortingNumber[] sort(SortingNumber[] data) {
-        for(int i = 0; i < data.length; i++){
+        for(int i = 0; i < data.length - 1; i++){
             int minimumIndex = i;
 
             for(int j = i+1; j < data.length; j++){
